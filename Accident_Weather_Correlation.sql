16a3dab1d2d7b787d6c852db63a9f1f498d5003a
HarshKothari <hk30598@gmail.com>
2020-04-17 16:14:58 -0700

    Delete Accident_Weather_Correlation.sql

--- Accident_Weather_Correlation.sql
+++
@@ -1,7 +0,0 @@
-SELECT acc.month, acc.Year , acc.total_acc ,  str.total_severe_whether 
-FROM `cs540-269500.Accident.total_acc_by_month` as acc, 
-`cs540-269500.Weather.total_storm_by_month` as str, 
-`cs540-269500.Weather.month_num_mapping` as mn
-WHERE str.month_name = mn.month_name
-  AND acc.Month = mn.month_number 
-  AND acc.Year = cast(str.year as string)
